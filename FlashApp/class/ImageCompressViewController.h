3670c0b1d89dc170512d0df4c2d44b2315526784
zhuguangtao <zhuguangtao1990@gmail.com>
2013-05-21 15:00:33 +0800

    5月21日备份提交

---
+++ FlashApp/class/ImageCompressViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageCompressViewController.h
+//  FlashApp
+//
+//  Created by 朱广涛 on 13-5-13.
+//  Copyright (c) 2013年 lidiansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageCompressViewController : UIViewController
+
+@end
---
+++ FlashApp/class/ImageCompressViewController.m
@@ -0,0 +1,38 @@
+//
+//  ImageCompressViewController.m
+//  FlashApp
+//
+//  Created by 朱广涛 on 13-5-13.
+//  Copyright (c) 2013年 lidiansen. All rights reserved.
+//
+
+#import "ImageCompressViewController.h"
+
+@interface ImageCompressViewController ()
+
+@end
+
+@implementation ImageCompressViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
